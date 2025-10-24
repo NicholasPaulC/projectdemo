3e8901bee760a662837c7c105a53dda2d9457587
Ethan Hunt <ethan_james_hunt@hotmail.com>
2025-10-24 15:34:03 +1100

    Updated Tests to all use Constraint Model

--- ConsoleApp/TestProject/UnitTest1.cs
+++ ConsoleApp/TestProject/UnitTest1.cs
@@ -39,9 +39,7 @@ namespace TestProject
         [Test]
         public void InvalidKey()
         {
-            IngredientList test = new();
-
-            Assert.DoesNotThrow(() => test = ingredients1.Subtract(ingredients2));
+            IngredientList test = ingredients1.Subtract(ingredients2);
             
             Assert.That(test.List.ContainsKey(IngredientName.MILK), Is.False);
         }
@@ -49,35 +47,23 @@ namespace TestProject
         [Test]
         public void Multiple()
         {
-            int multiples1 = 0;
+            int multiples = ingredients1.MultiplesOf(ingredients1);
 
-            Assert.DoesNotThrow(() => multiples1 = ingredients1.MultiplesOf(ingredients2));
-
-            Assert.Equals(multiples1, 1);
-
-            int multiples2 = 0;
-            Assert.DoesNotThrow(() => multiples2 = ingredients1.MultiplesOf(ingredients2));
-
-            Assert.Equals(multiples2, 2);
-
+            Assert.That(multiples, Is.EqualTo(1));
         }
 
         [Test]
         public void Subtract()
         {
-            IngredientList test = new();
+            IngredientList test = ingredients3.Subtract(ingredients1);
 
-            Assert.DoesNotThrow(() => test = ingredients3.Subtract(ingredients1));
-
-            Assert.Equals(test.List[IngredientName.EGG], 1);
+            Assert.That(test.List[IngredientName.EGG], Is.EqualTo(1));
         }
 
         [Test]
         public void SubtractRemoveNegativesAndZero()
         {
-            IngredientList test = new();
-
-            Assert.DoesNotThrow(() => test = ingredients1.Subtract(ingredients3));
+            IngredientList test = ingredients1.Subtract(ingredients3);
 
             Assert.That(test.List.ContainsKey(IngredientName.EGG), Is.False);
         }
@@ -85,11 +71,9 @@ namespace TestProject
         [Test]
         public void Multiply()
         {
-            IngredientList test = new();
+            IngredientList test = ingredients1.Multiply(2);
 
-            Assert.DoesNotThrow(() => test = ingredients1.Multiply(2));
-
-            Assert.Equals(test.List[IngredientName.EGG], 2);
+            Assert.That(test.List[IngredientName.EGG], Is.EqualTo(2));
         }
     }
 }
